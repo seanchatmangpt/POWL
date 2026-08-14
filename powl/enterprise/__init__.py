22f067be9dbc05b0895eac1cee2c19ea9ac8c92e
Sean Chatman <136349053+seanchatmangpt@users.noreply.github.com>
2026-08-14 15:09:49 -0700

    enterprise: add TOGAF execution surface

---
+++ powl/enterprise/__init__.py
@@ -0,0 +1,19 @@
+from .togaf import (
+    ADMPhase,
+    ArchitectureAdmissionError,
+    ArchitectureContract,
+    ArchitecturePlan,
+    CapabilityGap,
+    WorkPackage,
+    compile_togaf_plan,
+)
+
+__all__ = [
+    "ADMPhase",
+    "ArchitectureAdmissionError",
+    "ArchitectureContract",
+    "ArchitecturePlan",
+    "CapabilityGap",
+    "WorkPackage",
+    "compile_togaf_plan",
+]
